f7a9b9611510518b4c22e17e34001ddc59f04d1e
salahbltr <salaheddine.bellitir@gmail.com>
2023-12-01 15:11:11 -0800

    update

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,3 +1,13 @@
-#!/bin/bash
-# Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -sL "$1"
+#!/usr/bin/python3
+"""displays the value of the X-Request-Id variable found in
+the header of the response.
+"""
+
+
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        head = response.headers.get('X-Request-Id')
+        print(head)
